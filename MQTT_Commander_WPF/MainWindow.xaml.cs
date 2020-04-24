425305e7d3c4e3b7fd065385e06aa03bb4f8b6cb
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2020-04-24 16:25:10 +0200

    Projektdateien hinzufügen.

---
+++ MQTT_Commander_WPF/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿// https://github.com/eclipse/paho.mqtt.m2mqtt
+
+//https://gist.github.com/cwschroeder/7b5117dca561c01def041e7d4c6d2771
+
+// https://m2mqtt.wordpress.com/m2mqtt-and-amazon-aws-iot/
+
+
+// https://www.elektormagazine.de/news/mein-weg-ins-iot-4-mqtt
+
+// Tutorials for MVVM:
+// https://www.codeproject.com/Articles/165368/WPF-MVVM-Quick-Start-Tutorial
+// https://www.codeproject.com/Tips/813345/Basic-MVVM-and-ICommand-Usage-Example
+
+//How to create PFX file
+// https://gist.github.com/adrenalinehit/b33994a4d430b26747ac#file-converting-to-pfx-using-openssl
+// openssl pkcs12 -export -out YOURPFXFILE.pfx -inkey*****-private.pem.key -in *****-certificate.pem.crt
+
+
+using System;
+
+using System.Windows;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using FileStore;
+using MQTT_Client.ViewModels;
+
+
+// including the M2Mqtt Library
+
+//using uPLibrary.Networking.M2Mqtt;
+//using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace MQTT_Commander_WPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+
+        UI_ViewModel ViewModel = new UI_ViewModel();
+        
+        #region Region MainWindow
+        public MainWindow()
+        {
+            InitializeComponent();
+            base.DataContext = ViewModel;
+
+            Closing += ViewModel.OnWindowClosing;
+        }
+        #endregion
+
+        #region Region ValidateServerCertificate   (not used, only here to rememer how it works)
+        public static bool ValidateServerCertificate(
+        object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+
+            // Do not allow this client to communicate with unauthenticated servers.
+            return false;
+        }
+        #endregion
+
+
+        #region Region SelectLocalCertificate   (not used, only here to rememer how it works)
+        public static X509Certificate SelectLocalCertificate(
+        object sender,
+        string targetHost,
+        X509CertificateCollection localCertificates,
+        X509Certificate remoteCertificate,
+        string[] acceptableIssuers)
+        {
+            Console.WriteLine("Client is selecting a local certificate.");
+            if (acceptableIssuers != null &&
+                acceptableIssuers.Length > 0 &&
+                localCertificates != null &&
+                localCertificates.Count > 0)
+            {
+                // Use the first certificate that is from an acceptable issuer.
+                foreach (X509Certificate certificate in localCertificates)
+                {
+                    string issuer = certificate.Issuer;
+                    if (Array.IndexOf(acceptableIssuers, issuer) != -1)
+                        return certificate;
+                }
+            }
+            if (localCertificates != null &&
+                localCertificates.Count > 0)
+                return localCertificates[0];
+
+            return null;
+        }
+        #endregion
+
+
+        private void userCertificateValidationCallback(object sender)
+        {
+            Console.WriteLine("Certificate validated");
+        }
+ 
+
+        void setcolor()
+        {
+            btnPublish.Background = System.Windows.Media.Brushes.Green; 
+        }
+        // this code runs when the main window closes (end of the app)
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            App.Current.Shutdown();
+        }       
+    }
+}
+
+
+
+
+
+
---
+++ MQTT_Commander_WPF/Models/AccountMembers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTT_Client.Models
+{
+    public class AccountMembers
+
+    {
+        public object AccessInstance { get; set; }
+
+        public string Name { get; set; }
+        public bool Security { get; set; }
+        public string CaCertPath { get; set; }
+        public string ClientCertPath { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Port { get; set; }
+
+        public AccountMembers() { }
+        public AccountMembers(object pAccessInstance, string pName, bool pSecurity, string pCaCertPath, string pClientCertPat, string pUser, string pPassword, string pPort)
+        {
+            Name = pName;
+            AccessInstance = pAccessInstance;          
+            Security = pSecurity;
+            CaCertPath = pCaCertPath;
+            ClientCertPath = pClientCertPat;
+            User = pUser;
+            Password = pPassword;
+            Port = pPort;
+        }
+    }
+}
